f80e73fdcfa049964971334b8a32e2a864a90295
briandavidjoyner <brian@briandavidjoyner.com>
2016-11-18 08:48:51 -0500

    Updating cookie + URL check function

--- promobar.js
+++ promobar.js
@@ -1,33 +1,35 @@
 var profiler = {
 
 	settings : {
-		//cookieN : 'f_o',
-		coookieN :'dc_cookie',
+		cookieN :'dc_cookie',
 		cookieV : {
 			oh : {
 				fo: '',
 				to: '',
 				lo: ''
 			},
-			discount: {
+			d: {
 				fiftyOff : {
-					mark : '',
-					code : '50',
-					enable : false,
-					copy : ''
+					m : '?50off',
+					c : '50',
+					e : false,
 				},
 				twentyFiveOff : {
-					mark : '?25off',
-					code : 'NEW',
-					enable : false,
-					copy : 'Save $25 Off Your First Order Of $75+ <strong>Use Code : NEW</strong> at Checkout'
+					m : '?25off',
+					c : 'NEW',
+					e : false,
 				}
 			},
+		},
+		copy : {
+			fiftyOff : '',
+			twentyFiveOff : 'Save $25 Off Your First Order Of $75+ <strong>Use Code : NEW</strong> at Checkout'
+		},
 		default : {
 			code : '20OFF',
 			enable : true,
 			copy : 'Save 20% On All Orders Through Thanksgiving! <strong>Use Code : 20OFF</strong>'
-		}},
+		},
 		cookieE : 30,
     	urls_to_exclude : ['/checkout/thank_you','/pages/passport','/pages/huledet'],
     	css : '#promo_code_bar{line-height:200%;position:fixed;height:104px!important;width:100%;background-color:#6dc01d;z-index:-1000000000;display:table;}#text_bar{color:white;display:table-cell;vertical-align:middle;text-align:center;font-size:2em;}@media only screen and (max-width: 900px) {#promo_code_bar{line-height:200%;position:fixed;height:0px;width:100%;background-color:#6dc01d;z-index:-1000000;display:table;}#promo_code_bar{bottom:0!important;font-size:80%;padding:5px 5px 5px 5px;height:130px!important;}#text_bar{line-height:125%;}#sidebar, #content{margin-top:0px!important;}#footer{padding-bottom:200px!important;}}@media only screen and (min-width: 901px) {#sidebar, #content{margin-top:104px!important;padding-bottom:104px!important;}}',
@@ -56,32 +58,35 @@ var profiler = {
             c = c.substring(1);
         }
         if (c.indexOf(name) === 0) {
-            return c.substring(name.length,c.length);
+            var toReturn = c.substring(name.length,c.length);
+            return JSON.parse(toReturn);
         }
     	}
     	return false;
 	},
 
 	setCookie : function(cookieN,cookieV,cookieE){
+   		var toString = JSON.stringify(cookieV);
    		var d = new Date();
     	d.setTime(d.getTime() + (cookieE*24*60*60*1000));
     	var expires = "expires="+ d.toUTCString();
-    	document.cookie = cookieN + "=" + cookieV + ";" + expires + ";path=/";
-    	//console.log('setting cookie now');
+    	document.cookie = cookieN + "=" + toString + ";" + expires + ";path=/";
+    	console.log('setting cookie now');
 	},
 
 	discountArray : function(){
-		return Object.keys(this.settings.cookieV.discounts);
-	}
+		return Object.keys(this.settings.cookieV.d);
+	},
 
 	urlTcookie : function(){
-
+		var _this = profiler.settings;
 		var url = this.urlSearch();
 		var discountMark = this.discountArray();
-		discountMark.forEach(function(search){
-			if (url.indexOf(search) > -1){
-				this.settings.cookieV.discounts[search].enable = true;
-				return;
+		discountMark.some(function(search){
+			if (url.indexOf(profiler.settings.cookieV.d[search].m) > -1){
+				_this.cookieV.d[search].enable = true;
+				profiler.setCookie(_this.cookieN,_this.cookieV,_this.cookieE);
+				return search;
 			}
 		});
 	},
